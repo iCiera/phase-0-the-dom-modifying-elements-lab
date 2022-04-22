19f6c02f63cf8923fd42f77f318c5caf58d8fa1c
ciera <cieranicole.m15@gmail.com>
2022-04-22 16:52:22 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const element = document.querySelector('main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = 'Ciera is the champion'
+document.body.append(newHeader);
+
+
